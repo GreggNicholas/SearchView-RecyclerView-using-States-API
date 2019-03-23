ea0a26522c2e339ce0093f9542c4f75bd46a3bb9
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-23 12:11:23 -0400

    Adds recyclerview & adapter views to main

--- app/src/main/java/com/example/locationsearchviewwithretrofit/MainActivity.java
+++ app/src/main/java/com/example/locationsearchviewwithretrofit/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.locationsearchviewwithretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 
+import com.example.locationsearchviewwithretrofit.Controller.CountryAdapter;
 import com.example.locationsearchviewwithretrofit.Service.PatriotService;
 import com.example.locationsearchviewwithretrofit.Service.RetrofitSingleton;
 
@@ -13,10 +16,16 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
+    private SearchView searchView;
+    private RecyclerView recyclerView;
+    private CountryAdapter countryAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.main_searchview);
+        recyclerView = findViewById(R.id.main_recyclerview);
 
         Retrofit retrofit = RetrofitSingleton.getInstance();
         retrofit.create(PatriotService.class)
@@ -24,6 +33,11 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
+        countryAdapter = new CountryAdapter();
+        recyclerView.setAdapter(countryAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+
     }
 
 
