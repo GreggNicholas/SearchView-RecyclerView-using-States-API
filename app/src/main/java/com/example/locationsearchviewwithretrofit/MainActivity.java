1d15bef51b2a672a122971a841548c282639f951
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-23 10:23:02 -0400

    Implements Searchview in main

--- app/src/main/java/com/example/locationsearchviewwithretrofit/MainActivity.java
+++ app/src/main/java/com/example/locationsearchviewwithretrofit/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.locationsearchviewwithretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
     }
 }
